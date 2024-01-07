3eb0916047d527dee29088545aab0c1ff14b39dc
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-01-07 06:02:11 -0500

    get all and sigle item created

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
 app = FastAPI()
+from models import Todos
 
-TODO = []
+
+todo = []
 # get all todos 
 @app.get("/")
-async def home():
-	return {"Message": TODO}
+async def get_all_todos():
+	return {"Items": todo}
 
 # get single todos 
+@app.get("/{id}/")
+async def get_single_item(id:int):
+	restult = 0
+	for item in todo:
+		if item.id == id:
+			return item
+	return {"message": "Item not found"}
 
+# create new todos 
+@app.post("/")
+async def add_new_item(items:Todos):
+	todo.append(items)
+	return {"Message": f'Successfully added new items => {items}'}
 
 # update todos 
 
