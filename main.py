984fb41614f37ed9cd898c993d417f0151e1dfe2
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2024-01-07 06:18:45 -0500

    item creates, updated and deleted successfully

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI
 app = FastAPI()
 from models import Todos
 
-
 todo = []
+
 # get all todos 
 @app.get("/")
-async def get_all_todos():
+async def get_all_items():
 	return {"Items": todo}
 
 # get single todos 
 @app.get("/{id}/")
 async def get_single_item(id:int):
-	restult = 0
 	for item in todo:
 		if item.id == id:
 			return item
@@ -24,10 +23,23 @@ async def add_new_item(items:Todos):
 	todo.append(items)
 	return {"Message": f'Successfully added new items => {items}'}
 
-# update todos 
-
+# update todos  
+@app.put("/{id}/")
+async def update_item(id:int, items:Todos):
+	for obj in todo:
+		if obj.id == id:
+			obj.id = id
+			obj.item = items.item
+			return {"messages": f"Successfully updated item => {obj}"}
+	return {"message": "Item not found to update"}
 
 # delete todos 
-
+@app.delete("/{id}/")
+async def delete_item(id:int):
+	for item in todo:
+		if item.id == id:
+			todo.remove(item)
+			return {"message": f"Item deleted successfully => {item}"}
+	return {"message": "Item not found to remove"}
 
 
